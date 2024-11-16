1b6a193837dec89da24f266edf29664466a49bad
cristian <cristian.sw@proton.me>
2024-11-16 07:39:39 +0000

    Improved logging: - added date and time of run in log file name - all test runs are stored in same file

--- src/test/java/org/example/resources/Utils.java
+++ src/test/java/org/example/resources/Utils.java
@@ -6,19 +6,30 @@ import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Properties;
 
 public class Utils {
+    public static RequestSpecification rec;
 
     public RequestSpecification requestSpecification() throws IOException {
-        PrintStream ps = new PrintStream(new FileOutputStream("log.txt"));
-        return new RequestSpecBuilder()
-                .setBaseUri(getGlobalProperties("baseUrl"))
-                .addHeader("Content-Type","application/json")
-                .addQueryParam("key","qaclick123")
-                .addFilter(RequestLoggingFilter.logRequestTo(ps))
-                .addFilter(ResponseLoggingFilter.logResponseTo(ps))
-                .build();
+        if (rec == null) {
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formattedDateTime = currentDateTime.format(formatter);
+            PrintStream ps = new PrintStream(Files.newOutputStream(Paths.get("log_" + formattedDateTime + ".txt")));
+            rec = new RequestSpecBuilder()
+                    .setBaseUri(getGlobalProperties("baseUrl"))
+                    .addHeader("Content-Type", "application/json")
+                    .addQueryParam("key", "qaclick123")
+                    .addFilter(RequestLoggingFilter.logRequestTo(ps))
+                    .addFilter(ResponseLoggingFilter.logResponseTo(ps))
+                    .build();
+        }
+        return rec;
     }
 
     public String getGlobalProperties(String key) throws IOException {
