4805cbf5b91648fe4b32b65ec7455c5961bd1c2a
cristian <cristian.sw@proton.me>
2024-11-21 20:23:59 +0100

    Added jenkins docker compose config file Added report generation

--- src/test/java/org/example/cucumber/Options/TestRunner.java
+++ src/test/java/org/example/cucumber/Options/TestRunner.java
@@ -5,6 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/org/example/features", glue = {"org.example.stepDefinitions"}, tags = "@DeletePlace")
+@CucumberOptions(features = "src/test/java/org/example/features",plugin = "json:target/jsonReports/cucumber-report.json",glue = {"org.example.stepDefinitions"})
 public class TestRunner {
+//    , tags = "@DeletePlace"
 }
